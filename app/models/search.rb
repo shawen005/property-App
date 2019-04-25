e030f6485d64cf00e53394177c89dcf93fb2bffc
Aruna <arunaseun@yahoo.com>
2019-04-25 20:28:36 +0100

    fixing syntax error after deploying to heroku

--- app/models/search.rb
+++ app/models/search.rb
@@ -12,9 +12,9 @@ class Search < ApplicationRecord
 
  	lists = Listing.order(:location)
  	lists = lists.where("location like ?", "%#{areas} %") if areas.present?
- 	lists = lists.where(status:,status) if status.present?
- 	lists = lists.where("price <= ?", min_price) if min_price.present?
-    lists = lists.where("price <= ?", max_price) if max_price.present?
+ 	lists = lists.where(status:status) if status.present?
+ 	lists = lists.where("price <= ?" min_price) if min_price.present?
+    lists = lists.where("price <= ?" max_price) if max_price.present?
     lists
 
  
