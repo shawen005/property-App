8a811b4e0415a0d0f6465d6d58305a2b6b36fa9c
Aruna <arunaseun@yahoo.com>
2019-04-27 21:45:13 +0100

    removed bootstrap.js add bootstrap-sprockets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery2
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
 //= require cocoon
