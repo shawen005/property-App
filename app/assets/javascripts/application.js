e6686be1f34b05f1cfe7c1b976e2c61924a23ab1
Aruna <arunaseun@yahoo.com>
2019-04-27 23:19:07 +0100

    reordered application.js and move bootstrap ahead of jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery2
 //= require bootstrap-sprockets
+//= require jquery2
 //= require jquery_ujs
 //= require jquery-ui
 //= require cocoon
