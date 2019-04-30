ddf846e3c5fd6274d3e81fed3646bfd0b624ab0a
Aruna <arunaseun@yahoo.com>
2019-04-30 04:19:24 +0100

    fixed fontawesome issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap-sprockets
 //= require jquery2
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require jquery-ui
 //= require cocoon
 //= require gritter
